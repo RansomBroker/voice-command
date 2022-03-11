8f74854957032d6e11ae391a404624d6a720880d
RansomBroker <yadisoke@gmail.com>
2022-03-11 12:44:13 +0700

    first commit

---
+++ src/authconf.js
@@ -0,0 +1,10 @@
+const auth = {
+    "API_KEY" : "AIzaSyDEJ7Ew97GG_NbcQD-3e7E49xI9UGhRCWc",
+    "CLIENT_ID" : "872541853679-05omeg1cdguvglv00ftg6scvc37ku1df.apps.googleusercontent.com",
+    "DISCOVERY_DOCS": ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    "SCOPES": "https://www.googleapis.com/auth/spreadsheets",
+    "SHEETS_ID" : "1t3Cf914ZjReS_Ar7C8U3XI6qOXYtU46wLeXKCC1xR2M",
+    "SHEETS_RANGE": "Sheet1!A:B"
+}
+
+export default  auth;
